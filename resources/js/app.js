7edace0bacee0297b9ab85cfc6bccd73eca909e6
baconboy7 <baconboy7@users.noreply.github.com>
2019-03-30 12:01:02 -0600

    added basic layout. testing netlify forms

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,12 @@
-
-console.log(process.env);
+const axios = require('axios');
 
 let nasaURL = "https://api.nasa.gov/planetary/apod?api_key=" + process.env.MIX_NASA_API_KEY;
 
-console.log(nasaURL);
+axios.get(nasaURL)
+    .then(function (response) {
+        console.log(response);
+        document.querySelector('.header').style.backgroundImage = `url(${response.data.hdurl})`;
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
