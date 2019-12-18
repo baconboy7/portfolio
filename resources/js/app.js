73d11cc82f25c6f19defa9f97d739fa56cf3518a
Ryan Fitzpatrick <rfitzpatrick@calldrip.com>
2019-12-18 09:36:37 -0700

    Added under construction message
    Added under construction toast

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,46 +1,50 @@
 const axios = require('axios');
 const loadImage = require('image-promise');
+const toastr = require('toastr');
 
-let nasaURL = "https://api.nasa.gov/planetary/apod?api_key=" + process.env.MIX_NASA_API_KEY;
+let nasaURL = 'https://api.nasa.gov/planetary/apod?api_key=' + process.env.MIX_NASA_API_KEY;
 
 axios.get(nasaURL)
-    .then(function (response) {
-        document.querySelector('.header').style.backgroundImage = `url(${response.data.hdurl})`;
+     .then(function (response) {
+         document.querySelector('.header').style.backgroundImage = `url(${response.data.hdurl})`;
+    
+         loadImage(response.data.hdurl)
+           .then(function () {
+               removeRocket();
+          
+               toastr.error('This site is still under construction!', 'Warning!');
+           })
+           .catch(function () {
+               removeRocket();
+           });
+    
+     })
+     .catch(function (error) {
+         console.log(error);
+     });
 
-        loadImage(response.data.hdurl)
-            .then(function() {
-                    removeRocket();
-                }
-            )
-            .catch(function () {
-                removeRocket();
-            })
-
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-
-function closeNavigation(){
+function closeNavigation ()
+{
     document.getElementById('navigation-toggle').checked = false;
 }
 
-function removeRocket(){
-    document.getElementById('cover').classList.add("vanish");
-    let rocket = document.getElementById("rocket");
+function removeRocket ()
+{
+    document.getElementById('cover').classList.add('vanish');
+    let rocket = document.getElementById('rocket');
     rocket.parentNode.removeChild(rocket);
 }
 
-document.getElementById("about-link").addEventListener('click', () => {
+document.getElementById('about-link').addEventListener('click', () => {
     closeNavigation();
 });
-document.getElementById("languages-link").addEventListener('click', () => {
+document.getElementById('languages-link').addEventListener('click', () => {
     closeNavigation();
 });
-document.getElementById("portfolio-link").addEventListener('click', () => {
+document.getElementById('portfolio-link').addEventListener('click', () => {
     closeNavigation();
 });
-document.getElementById("contact-link").addEventListener('click', () => {
+document.getElementById('contact-link').addEventListener('click', () => {
     closeNavigation();
 });
 
